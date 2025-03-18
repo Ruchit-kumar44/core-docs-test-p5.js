0a6e3d2f7322f6a5fc16b22168159f246526386f
Ruchit-kumar44 <ruchitk539@gmail.comgit config --listgit config --global user.email ruchitk539@gmail.com>
2025-03-18 08:23:28 +0530

    Initial commit with JSDoc and GitHub Action

---
+++ src/example.js
@@ -0,0 +1,9 @@
+/**
+ * This function draws a circle.
+ * @param {number} x - The x-coordinate.
+ * @param {number} y - The y-coordinate.
+ * @param {number} radius - The radius of the circle.
+ */
+function drawCircle(x, y, radius) {
+    ellipse(x, y, radius);
+}
